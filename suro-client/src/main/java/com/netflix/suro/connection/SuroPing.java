b2e234183ae943ee840b73ac1ccf4cd909bed390
Zach Dexter <zdexter@thebrighttag.com>
2014-03-10 15:39:01 -0500

    added ping class for checking if suro-server is alive

---
+++ suro-client/src/main/java/com/netflix/suro/connection/SuroPing.java
@@ -0,0 +1,42 @@
+package com.netflix.suro.connection;
+
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransportException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.AbstractLoadBalancerPing;
+import com.netflix.loadbalancer.Server;
+
+/**
+ * Simple implementation for checking if a single server is alive
+ *
+ * @author zdexter
+ *
+ */
+public class SuroPing extends AbstractLoadBalancerPing {
+	private static Logger logger = LoggerFactory
+            .getLogger(SuroPing.class);
+
+    public SuroPing() {
+    }
+
+    public boolean isAlive(Server server) {
+        TSocket socket = new TSocket(server.getHost(), server.getPort(), 2000);
+        TFramedTransport transport = new TFramedTransport(socket);
+
+        try {
+                        logger.info("checking if server is alive...");
+			transport.open();
+			return true;
+		} catch (TTransportException e) {
+			return false;
+		}
+    }
+
+    @Override
+    public void initWithNiwsConfig(IClientConfig clientConfig) {
+    }
+}
