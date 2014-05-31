512e1183b59c1e4145e165c91e3db273dc599dfd
thinker0 <thinker0@gmail.com>
2014-06-01 08:44:56 +0900

    lake connection suroPing fix

--- suro-client/src/main/java/com/netflix/suro/connection/SuroPing.java
+++ suro-client/src/main/java/com/netflix/suro/connection/SuroPing.java
@@ -1,40 +1,57 @@
 package com.netflix.suro.connection;
 
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.AbstractLoadBalancerPing;
+import com.netflix.loadbalancer.Server;
 import org.apache.thrift.transport.TFramedTransport;
 import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.netflix.client.config.IClientConfig;
-import com.netflix.loadbalancer.AbstractLoadBalancerPing;
-import com.netflix.loadbalancer.Server;
+import javax.annotation.Nullable;
+import java.net.SocketException;
 
 /**
  * Simple implementation for checking if a single server is alive
  *
  * @author zdexter
- *
  */
 public class SuroPing extends AbstractLoadBalancerPing {
-	private static Logger logger = LoggerFactory
+    private static Logger logger = LoggerFactory
             .getLogger(SuroPing.class);
 
     public SuroPing() {
     }
 
-    public boolean isAlive(Server server) {
-        TSocket socket = new TSocket(server.getHost(), server.getPort(), 2000);
-        TFramedTransport transport = new TFramedTransport(socket);
+    private static void close(@Nullable TTransport transport) {
+        if (transport == null) {
+            return;
+        }
+        transport.close();
+    }
 
+    public boolean isAlive(Server server) {
+        TSocket socket = null;
+        TFramedTransport transport = null;
         try {
-			transport.open();
-			return true;
-		} catch (TTransportException e) {
-			return false;
+            socket = new TSocket(server.getHost(), server.getPort(), 2000);
+            socket.getSocket().setTcpNoDelay(true);
+            socket.getSocket().setKeepAlive(true);
+            socket.getSocket().setSoLinger(true, 0);
+            transport = new TFramedTransport(socket);
+            transport.open();
+            return true;
+        } catch (TTransportException e) {
+            logger.warn("Ping {}", e.getMessage());
+            return false;
+        } catch (SocketException e) {
+            logger.warn("Ping {}", e.getMessage());
+            return false;
         } finally {
-            transport.close();
-            socket.close();
+            close(transport);
+            close(socket);
         }
     }
 
---
+++ suro-client/src/test/java/com/netflix/suro/connection/TestSuroPing.java
@@ -0,0 +1,30 @@
+package com.netflix.suro.connection;
+
+import com.netflix.loadbalancer.Server;
+import com.netflix.suro.SuroServer4Test;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author thinker0
+ */
+public class TestSuroPing {
+
+    @Test
+    public void pingTest() throws Exception {
+        final SuroServer4Test server4Test = new SuroServer4Test(7901);
+        server4Test.start();
+        SuroPing ping = new SuroPing();
+        Server server = new Server("localhost", 7901);
+        assertEquals(true, ping.isAlive(server));
+        server4Test.shutdown();
+    }
+
+    @Test
+    public void pingFailTest() throws Exception {
+        SuroPing ping = new SuroPing();
+        Server server = new Server("localhost", 7901);
+        assertEquals(false, ping.isAlive(server));
+    }
+}
