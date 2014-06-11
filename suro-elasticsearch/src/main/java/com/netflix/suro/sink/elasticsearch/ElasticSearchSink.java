80746b41c337e5a92554289154afe661115ed9ba
Jae Hyeon Bae <metacret@gmail.com>
2014-06-11 15:12:36 -0700

    suro-elasticsearch

---
+++ suro-elasticsearch/src/main/java/com/netflix/suro/sink/elasticsearch/ElasticSearchSink.java
@@ -0,0 +1,188 @@
+package com.netflix.suro.sink.elasticsearch;
+
+import com.fasterxml.jackson.annotation.JacksonInject;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.servo.annotations.DataSourceType;
+import com.netflix.servo.annotations.Monitor;
+import com.netflix.servo.monitor.Monitors;
+import com.netflix.suro.message.Message;
+import com.netflix.suro.message.MessageContainer;
+import com.netflix.suro.queue.MemoryQueue4Sink;
+import com.netflix.suro.queue.MessageQueue4Sink;
+import com.netflix.suro.sink.QueuedSink;
+import com.netflix.suro.sink.Sink;
+import org.elasticsearch.action.bulk.BulkItemResponse;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.Requests;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ElasticSearchSink extends QueuedSink implements Sink {
+    private static Logger log = LoggerFactory.getLogger(ElasticSearchSink.class);
+
+    public static final String TYPE = "elasticsearch";
+
+    private Client client;
+    private final List<String> addressList;
+    private final IndexInfoBuilder indexInfo;
+    private final DiscoveryClient discoveryClient;
+    private final Settings settings;
+
+    @Monitor(name = "indexedRowCount", type = DataSourceType.COUNTER)
+    private long indexedRowCount = 0;
+
+    @Monitor(name = "parsingFailedRowCount", type = DataSourceType.COUNTER)
+    private long parsingFailedRowCount = 0;
+
+    @Monitor(name = "rejectedRowCount", type = DataSourceType.COUNTER)
+    private long rejectedRowCount = 0;
+
+    @Monitor(name = "indexDelay", type = DataSourceType.GAUGE)
+    private long indexDelay = 0;
+
+    @JsonCreator
+    public ElasticSearchSink(
+            @JsonProperty("queue4Sink") MessageQueue4Sink queue4Sink,
+            @JsonProperty("batchSize") int batchSize,
+            @JsonProperty("batchTimeout") int batchTimeout,
+            @JsonProperty("cluster.name") String clusterName,
+            @JsonProperty("client.transport.sniff") Boolean sniff,
+            @JsonProperty("client.transport.ping_timeout") String pingTimeout,
+            @JsonProperty("client.transport.nodes_sampler_interval") String nodesSamplerInterval,
+            @JsonProperty("addressList") List<String> addressList,
+            @JacksonInject @JsonProperty("indexInfo") IndexInfoBuilder indexInfo,
+            @JacksonInject DiscoveryClient discoveryClient,
+            @JacksonInject ObjectMapper jsonMapper,
+            @JacksonInject Client client) {
+        this.indexInfo =
+                indexInfo == null ? new DefaultIndexInfoBuilder(null, null, null, null, null, jsonMapper) : indexInfo;
+
+        initialize(queue4Sink == null ? new MemoryQueue4Sink(10000) : queue4Sink, batchSize, batchTimeout);
+
+        ImmutableSettings.Builder settingsBuilder = ImmutableSettings.settingsBuilder();
+        if (clusterName != null) {
+            settingsBuilder = settingsBuilder.put("cluster.name", clusterName);
+        } else {
+            clusterName = "NA";
+            settingsBuilder = settingsBuilder.put("client.transport.ignore_cluster_name", true);
+        }
+
+        if (sniff != null) {
+            settingsBuilder = settingsBuilder.put("client.transport.sniff", sniff);
+        }
+        if (pingTimeout != null) {
+            settingsBuilder = settingsBuilder.put("client.transport.ping_timeout", pingTimeout);
+        }
+        if (nodesSamplerInterval != null) {
+            settingsBuilder = settingsBuilder.put("client.transport.nodes_sampler_interval", nodesSamplerInterval);
+        }
+
+        this.client = client;
+        this.settings = settingsBuilder.build();
+        this.discoveryClient = discoveryClient;
+        this.addressList = addressList;
+
+        Monitors.registerObject(clusterName, this);
+    }
+
+    @Override
+    public void writeTo(MessageContainer message) {
+        enqueue(message.getMessage());
+    }
+
+    @Override
+    public void open() {
+        if (client == null) {
+            client = new TransportClient(settings);
+            if (discoveryClient != null) {
+                for (String address : addressList) {
+                    String[] host_port = address.split(":");
+
+                    List<InstanceInfo> listOfinstanceInfo = discoveryClient.getInstancesByVipAddress(host_port[0], false);
+                    for (InstanceInfo ii : listOfinstanceInfo) {
+                        if (ii.getStatus().equals(InstanceInfo.InstanceStatus.UP)) {
+                            ((TransportClient)client).addTransportAddress(
+                                    new InetSocketTransportAddress(ii.getHostName(), Integer.parseInt(host_port[1])));
+                        }
+                    }
+                }
+            } else {
+                for (String address : addressList) {
+                    String[] host_port = address.split(":");
+                    ((TransportClient)client).addTransportAddress(
+                            new InetSocketTransportAddress(host_port[0], Integer.parseInt(host_port[1])));
+                }
+            }
+        }
+
+        setName(ElasticSearchSink.class.getSimpleName() + "-" + settings.get("cluster.name"));
+        start();
+    }
+
+    @Override
+    public String recvNotice() { return null; }
+
+    @Override
+    public String getStat() {
+        return String.format("indexed: %d, rejected: %d, parsing failed: %d",
+                indexedRowCount, rejectedRowCount, parsingFailedRowCount);
+    }
+
+    @Override
+    protected void beforePolling() throws IOException {}
+
+    @Override
+    protected void write(List<Message> msgList) throws IOException {
+        BulkRequestBuilder request = client.prepareBulk();
+        for (Message m : msgList) {
+            IndexInfo info = indexInfo.create(m);
+            if (info == null) {
+                ++parsingFailedRowCount;
+            } else {
+                request.add(Requests.indexRequest(info.getIndex())
+                        .type(info.getType())
+                        .source(info.getSource())
+                        .id(info.getId())
+                        .opType(IndexRequest.OpType.CREATE));
+            }
+        }
+
+        BulkResponse response = request.execute().actionGet();
+        if (response.hasFailures()) {
+            int rejectedCount = 0;
+            for (BulkItemResponse r : response.getItems()) {
+                if (r.isFailed() && !r.getFailureMessage().startsWith("DocumentAlreadyExistsException")) {
+                    log.error("Failed with: " + r.getFailureMessage());
+                    ++rejectedCount;
+
+                    enqueue(msgList.get(r.getItemId()));
+                }
+            }
+            rejectedRowCount += rejectedCount;
+            indexedRowCount += request.numberOfActions() - rejectedCount;
+        } else {
+            indexedRowCount += request.numberOfActions();
+        }
+
+        indexDelay = System.currentTimeMillis() - indexInfo.create(msgList.get(0)).getTimestamp();
+    }
+
+    @Override
+    protected void innerClose() throws IOException {
+        client.close();
+    }
+}
---
+++ suro-elasticsearch/src/test/java/com/netflix/suro/sink/elasticsearch/TestDefaultIndexInfoBuilder.java
@@ -0,0 +1,264 @@
+package com.netflix.suro.sink.elasticsearch;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.BeanProperty;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.InjectableValues;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.NamedType;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.netflix.suro.jackson.DefaultObjectMapper;
+import com.netflix.suro.message.Message;
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class TestDefaultIndexInfoBuilder {
+    private ObjectMapper jsonMapper = new DefaultObjectMapper();
+
+    @Before
+    public void setup() {
+        System.setProperty("user.timezone", "GMT");
+    }
+
+    @Test
+    public void shouldReturnNullOnParsingFailure() {
+        DefaultIndexInfoBuilder builder = new DefaultIndexInfoBuilder(
+                null,
+                null,
+                null,
+                null,
+                null,
+                jsonMapper);
+
+        assertNull(builder.create(new Message("routingkey", "message".getBytes())));
+    }
+
+    @Test
+    public void shouldNullOrEmptyIndexTypeMapReturnRoutingKey() throws JsonProcessingException {
+        DefaultIndexInfoBuilder builder = new DefaultIndexInfoBuilder(
+                null,
+                null,
+                null,
+                null,
+                null,
+                jsonMapper);
+
+        Map<String, Object> msg = new ImmutableMap.Builder<String, Object>().put("f1", "v1").build();
+
+        IndexInfo info = builder.create(new Message("routingkey", jsonMapper.writeValueAsBytes(msg)));
+        assertEquals(info.getIndex(), "routingkey");
+        assertEquals(info.getType(), "default");
+    }
+
+    @Test
+    public void shouldIndexTypeMapReturnSetting() throws JsonProcessingException {
+        DefaultIndexInfoBuilder builder = new DefaultIndexInfoBuilder(
+                new ImmutableMap.Builder<String, String>()
+                        .put("routingkey1", "index1:type1")
+                        .put("routingkey2", "index2").build(),
+                null,
+                null,
+                null,
+                null,
+                jsonMapper);
+
+        Map<String, Object> msg = new ImmutableMap.Builder<String, Object>().put("f1", "v1").build();
+
+        IndexInfo info = builder.create(new Message("routingkey1", jsonMapper.writeValueAsBytes(msg)));
+        assertEquals(info.getIndex(), "index1");
+        assertEquals(info.getType(), "type1");
+
+        info = builder.create(new Message("routingkey2", jsonMapper.writeValueAsBytes(msg)));
+        assertEquals(info.getIndex(), "index2");
+        assertEquals(info.getType(), "default");
+    }
+
+    @Test
+    public void shouldIndexFormatterWorkWithTimestampField() throws JsonProcessingException {
+        Properties props = new Properties();
+        props.put("dateFormat", "YYYYMMdd");
+
+        DefaultIndexInfoBuilder builder = new DefaultIndexInfoBuilder(
+                new ImmutableMap.Builder<String, String>()
+                        .put("routingkey1", "index1:type1")
+                        .put("routingkey2", "index2").build(),
+                null,
+                new TimestampField("ts", null),
+                new IndexSuffixFormatter("date", props),
+                null,
+                jsonMapper);
+
+        DateTime dt = new DateTime("2014-10-12T00:00:00.000Z");
+        Map<String, Object> msg = new ImmutableMap.Builder<String, Object>()
+                .put("ts", dt.getMillis())
+                .put("f1", "v1").build();
+
+        IndexInfo info = builder.create(new Message("routingkey1", jsonMapper.writeValueAsBytes(msg)));
+        assertEquals(info.getIndex(), "index120141012");
+        assertEquals(info.getType(), "type1");
+
+        info = builder.create(new Message("routingkey2", jsonMapper.writeValueAsBytes(msg)));
+        assertEquals(info.getIndex(), "index220141012");
+        assertEquals(info.getType(), "default");
+    }
+
+    @Test
+    public void shouldSourceConvertedOrNot() throws IOException {
+        DataConverter converter = new DataConverter() {
+            @Override
+            public Map<String, Object> convert(Map<String, Object> msg) {
+                msg.put("app", "app");
+                return msg;
+            }
+        };
+
+        DefaultIndexInfoBuilder builder = new DefaultIndexInfoBuilder(
+                null,
+                null,
+                null,
+                null,
+                converter,
+                jsonMapper);
+
+        Map<String, Object> msg = new ImmutableMap.Builder<String, Object>()
+                .put("f1", "v1").build();
+
+        IndexInfo info = builder.create(new Message("routingkey", jsonMapper.writeValueAsBytes(msg)));
+        Map<String, Object> source = jsonMapper.readValue(info.getSource(), new TypeReference<Map<String, Object>>() {});
+        assertEquals(source.get("app"), "app");
+        assertEquals(source.get("f1"), "v1");
+
+        builder = new DefaultIndexInfoBuilder(
+                null,
+                null,
+                null,
+                null,
+                null,
+                jsonMapper);
+
+        msg = new ImmutableMap.Builder<String, Object>()
+                .put("f1", "v1").build();
+
+        info = builder.create(new Message("routingkey", jsonMapper.writeValueAsBytes(msg)));
+        source = jsonMapper.readValue(info.getSource(), new TypeReference<Map<String, Object>>() {});
+        assertNull(source.get("app"));
+        assertEquals(source.get("f1"), "v1");
+    }
+
+    @Test
+    public void shouldGetIdReturnNullOnEmptyList() throws JsonProcessingException {
+        DefaultIndexInfoBuilder builder = new DefaultIndexInfoBuilder(
+                null,
+                null,
+                null,
+                null,
+                null,
+                jsonMapper);
+
+        Map<String, Object> msg = new ImmutableMap.Builder<String, Object>()
+                .put("f1", "v1").build();
+
+
+        IndexInfo info = builder.create(new Message("routingkey", jsonMapper.writeValueAsBytes(msg)));
+        assertNull(info.getId());
+
+        builder = new DefaultIndexInfoBuilder(
+                null,
+                new ArrayList<String>(),
+                null,
+                null,
+                null,
+                jsonMapper);
+        info = builder.create(new Message("routingkey", jsonMapper.writeValueAsBytes(msg)));
+        assertNull(info.getId());
+    }
+
+    @Test
+    public void shouldGetIdReturnsConcatenatedStr() throws JsonProcessingException {
+        Map<String, Object> msg = new ImmutableMap.Builder<String, Object>()
+                .put("f1", "v1")
+                .put("f2", "v2")
+                .put("f3", "v3")
+                .build();
+
+        DefaultIndexInfoBuilder builder = new DefaultIndexInfoBuilder(
+                null,
+                Lists.newArrayList("f1", "f2"),
+                null,
+                null,
+                null,
+                jsonMapper);
+        IndexInfo info = builder.create(new Message("routingkey", jsonMapper.writeValueAsBytes(msg)));
+        assertEquals(info.getId(), "v1v2");
+    }
+
+    @Test
+    public void shouldGetIdReturnsConcatedStrWithTimeslice() throws JsonProcessingException {
+        DateTime dt = new DateTime("2014-10-12T12:12:12.000Z");
+
+        Map<String, Object> msg = new ImmutableMap.Builder<String, Object>()
+                .put("f1", "v1")
+                .put("f2", "v2")
+                .put("f3", "v3")
+                .put("ts", dt.getMillis())
+                .build();
+
+        DefaultIndexInfoBuilder builder = new DefaultIndexInfoBuilder(
+                null,
+                Lists.newArrayList("f1", "f2", "ts_minute"),
+                new TimestampField("ts", null),
+                null,
+                null,
+                jsonMapper);
+        IndexInfo info = builder.create(new Message("routingkey", jsonMapper.writeValueAsBytes(msg)));
+        assertEquals(info.getId(), ("v1v2" + dt.getMillis() / 60000));
+    }
+
+    @Test
+    public void testCreation() throws IOException {
+        String desc = "{\n" +
+                "    \"type\": \"default\",\n" +
+                "    \"indexTypeMap\":{\"routingkey1\":\"index1:type1\", \"routingkey2\":\"index2:type2\"},\n" +
+                "    \"idFields\":[\"f1\", \"f2\", \"ts_minute\"],\n" +
+                "    \"timestamp\": {\"field\":\"ts\"},\n" +
+                "    \"indexSuffixFormatter\":{\"type\": \"date\", \"properties\":{\"dateFormat\":\"YYYYMMdd\"}}\n" +
+                "}";
+        jsonMapper.setInjectableValues(new InjectableValues() {
+                    @Override
+                    public Object findInjectableValue(
+                            Object valueId,
+                            DeserializationContext ctxt,
+                            BeanProperty forProperty,
+                            Object beanInstance
+                    ) {
+                        if (valueId.equals(ObjectMapper.class.getCanonicalName())) {
+                            return jsonMapper;
+                        } else {
+                            return null;
+                        }
+                    }
+                });
+        DateTime dt = new DateTime("2014-10-12T12:12:12.000Z");
+
+        Map<String, Object> msg = new ImmutableMap.Builder<String, Object>()
+                .put("f1", "v1")
+                .put("f2", "v2")
+                .put("f3", "v3")
+                .put("ts", dt.getMillis())
+                .build();
+        IndexInfoBuilder builder = jsonMapper.readValue(desc, new TypeReference<IndexInfoBuilder>(){});
+        IndexInfo info = builder.create(new Message("routingkey", jsonMapper.writeValueAsBytes(msg)));
+        assertEquals(info.getId(), ("v1v2" + dt.getMillis() / 60000));
+    }
+}
