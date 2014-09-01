0aac6fa651ebe37dc971394a2fd07caeeb3cefc6
Jae Hyeon Bae <metacret@gmail.com>
2014-09-01 00:39:47 -0700

    suro-tranquility is removed

--- suro-server/src/main/java/com/netflix/suro/SuroModule.java
+++ suro-server/src/main/java/com/netflix/suro/SuroModule.java
@@ -22,10 +22,10 @@ import com.google.inject.AbstractModule;
 import com.netflix.suro.aws.PropertyAWSCredentialsProvider;
 import com.netflix.suro.input.thrift.MessageSetProcessor;
 import com.netflix.suro.input.thrift.ServerConfig;
+import com.netflix.suro.jackson.DefaultObjectMapper;
 import com.netflix.suro.routing.RoutingMap;
 import com.netflix.suro.server.StatusServer;
 import com.netflix.suro.sink.SinkManager;
-import com.netflix.suro.sink.tranquility.ObjectMapperProvider;
 
 /**
  * Guice module for binding {@link AWSCredentialsProvider},
@@ -37,7 +37,7 @@ import com.netflix.suro.sink.tranquility.ObjectMapperProvider;
 public class SuroModule extends AbstractModule {
     @Override
     protected void configure() {
-        bind(ObjectMapper.class).toProvider(ObjectMapperProvider.class).asEagerSingleton();
+        bind(ObjectMapper.class).to(DefaultObjectMapper.class).asEagerSingleton();
         bind(AWSCredentialsProvider.class).to(PropertyAWSCredentialsProvider.class);
         bind(SuroService.class).asEagerSingleton();
         bind(StatusServer.class).asEagerSingleton();
